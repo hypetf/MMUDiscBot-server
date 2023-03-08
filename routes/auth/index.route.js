65ffe30234110d583c38dc08f251bd07738a38d3
Rabbani Khan <49875228+hypetf@users.noreply.github.com>
2023-03-08 20:32:35 +0000

    no biggie

--- routes/auth/index.route.js
+++ routes/auth/index.route.js
@@ -7,7 +7,7 @@ const {
 } = require('../../controllers/auth/authRedirect.controller');
 
 router.get('/redirect', authRedirectController);
-router.get('/login', (req, res) => {res.redirect('https://discord.com/api/oauth2/authorize?client_id=1082878003576582164&redirect_uri=http%3A%2F%2Flocalhost%3A5000%2Fapi%2Fauth%2Fredirect&response_type=code&scope=identify%20guilds');})
+router.get('/login', (req, res) => {res.redirect(`https://discord.com/api/oauth2/authorize?client_id=${process.env.DISCORD_CLIENT_ID}&redirect_uri=http%3A%2F%2Flocalhost%3A5000%2Fapi%2Fauth%2Fredirect&response_type=code&scope=identify%20guilds`);})
 router.get('/user', getAuthenticatedUser);
 router.get('/logout', logout);
 

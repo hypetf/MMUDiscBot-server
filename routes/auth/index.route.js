d0cd2330dd167f1032306ee097a87e576c5c2a55
Rabbani Khan <49875228+hypetf@users.noreply.github.com>
2023-04-30 21:44:42 +0100

    added revoke access token route

--- routes/auth/index.route.js
+++ routes/auth/index.route.js
@@ -4,12 +4,16 @@ const router = express.Router();
 const {
     authRedirectController,
     getAuthenticatedUser,
-    logout
+    logout,
+    revokeAT
 } = require('../../controllers/auth/authRedirect.controller');
 
 router.get('/redirect', authRedirectController);
 router.get('/login', (req, res) => {res.redirect(`https://discord.com/api/oauth2/authorize?client_id=${process.env.DISCORD_CLIENT_ID}&redirect_uri=http%3A%2F%2Flocalhost%3A5000%2Fapi%2Fauth%2Fredirect&response_type=code&scope=identify%20guilds`);})
 router.get('/user', getAuthenticatedUser);
 router.get('/logout', logout);
+router.get('/revoke', (req, res) => {
+    revokeAT();
+});
 
 module.exports = router;
